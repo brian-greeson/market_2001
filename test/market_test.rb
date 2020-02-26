74b9990eadf311d717ae7f79aa680df3fd725a67
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-26 11:18:43 -0700

    Begin Iteration3

--- test/market_test.rb
+++ test/market_test.rb
@@ -18,10 +18,8 @@ class MarketTest < Minitest::Test
 
     @vendor1.stock(@item1, 35)
     @vendor1.stock(@item2, 7)
-
     @vendor2.stock(@item4, 50)
     @vendor2.stock(@item3, 25)
-
     @vendor3.stock(@item1, 65)
   end
 
@@ -69,58 +67,3 @@ class MarketTest < Minitest::Test
   end
 
 end
-
-
-
-# pry(main)> market.vendors_that_sell(item1)
-# #=> [#<VENDOR 1...>, #VENDOR 3...>]
-#
-# pry(main)> market.vendors_that_sell(item4)
-# #=> [#<VENDOR 2...>]
-#
-# pry(main)> vendor1.potential_revenue
-# #=> 29.75
-#
-# pry(main)> vendor2.potential_revenue
-# #=> 345.00
-#
-# pry(main)> vendor3.potential_revenue
-# #=> 48.75
-
-
-
-
-
-# pry(main)> market = Market.new("South Pearl Street Farmers Market")
-# #=> #<Market:0x00007fe134933e20...>
-
-# pry(main)> vendor1 = Vendor.new("Rocky Mountain Fresh")
-# #=> #<VENDOR 1...>
-#
-# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# pry(main)> item2 = Item.new({name: 'Tomato', price: '$0.50'})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-# pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-#
-# pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-#
-# pry(main)> vendor1.stock(item1, 35)
-#
-# pry(main)> vendor1.stock(item2, 7)
-#
-# pry(main)> vendor2 = Vendor.new("Ba-Nom-a-Nom")
-# #=> #<VENDOR 2...>
-#
-# pry(main)> vendor2.stock(item4, 50)
-#
-# pry(main)> vendor2.stock(item3, 25)
-#
-# pry(main)> vendor3 = Vendor.new("Palisade Peach Shack")
-# #=> #VENDOR 3...>
-#
-# pry(main)> vendor3.stock(item1, 65)
