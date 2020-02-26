7f8f860b7b8dc84eb01d2403ebe1ee9a73f540a7
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-26 10:41:08 -0700

    Add starts with no vendors test

--- test/market_test.rb
+++ test/market_test.rb
@@ -6,34 +6,45 @@ class MarketTest < Minitest::Test
 
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    @vendor1.stock(item1, 35)
+    @vendor1.stock(item2, 7)
+
+    @vendor2.stock(item4, 50)
+    @vendor2.stock(item3, 25)
+
+    @vendor3.stock(item1, 65)
   end
 
   def test_it_exists
     assert_instance_of Market, @market
   end
 
+  def test_it_has_a_name
+    assert_equal "South Pearl Street Farmers Market", @market.name
+  end
+
+  def test_it_starts_with_no_vendors
+    assert_equal ([]), @market.vendors
+  end
+
+
+
 
 end
 
-#
-# pry(main)> require './lib/item'
-# #=> true
-#
-# pry(main)> require './lib/vendor'
-# #=> true
-#
-# pry(main)> require './lib/market'
-# #=> true
-#
 # pry(main)> market = Market.new("South Pearl Street Farmers Market")
 # #=> #<Market:0x00007fe134933e20...>
-#
-# pry(main)> market.name
-# #=> "South Pearl Street Farmers Market"
-#
-# pry(main)> market.vendors
-# #=> []
-#
+
 # pry(main)> vendor1 = Vendor.new("Rocky Mountain Fresh")
 # #=> #<Vendor:0x00007fe1348a1160...>
 #
