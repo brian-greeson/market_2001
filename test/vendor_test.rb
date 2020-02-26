9ce2ea85308f4918dc6ae2fd5eac4e8240147422
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-26 10:19:42 -0700

    Add vendor tests

---
+++ test/vendor_test.rb
@@ -0,0 +1,48 @@
+require "minitest/autorun"
+require "./lib/item"
+require "./lib/vendor"
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_a_name
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+
+  def test_it_starts_with_empty_inventory
+    assert_equal ({}), @vendor.inventory
+  end
+
+end
+
+
+
+
+# pry(main)> vendor.check_stock(item1)
+# #=> 0
+#
+# pry(main)> vendor.stock(item1, 30)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 30
+#
+# pry(main)> vendor.stock(item1, 25)
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 55
+#
+# pry(main)> vendor.stock(item2, 12)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
